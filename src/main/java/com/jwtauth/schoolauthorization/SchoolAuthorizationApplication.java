752ca6a41cd375b1d0966b9633194cff7b4ee4ae
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-26 18:06:45 +0530

    initial commit

---
+++ src/main/java/com/jwtauth/schoolauthorization/SchoolAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.jwtauth.schoolauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtauth/schoolauthorization/SchoolAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtauth.schoolauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
