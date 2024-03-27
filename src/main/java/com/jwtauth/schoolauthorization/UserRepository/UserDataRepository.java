38979a14d011b1673560658195bd2dbcea1775f5
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-27 13:55:25 +0530

    findUserByUsername method declare in UserDataRepository & corresponding code for that method is wrriten in UserMapper.xml

--- src/main/java/com/jwtauth/schoolauthorization/UserRepository/UserDataRepository.java
+++ src/main/java/com/jwtauth/schoolauthorization/UserRepository/UserDataRepository.java
@@ -1,8 +1,8 @@
 package com.jwtauth.schoolauthorization.UserRepository;
-
-import com.jwtauth.schoolauthorization.config.ManagementDataSource.ManagementDatabaseConnMapper;
+import com.jwtauth.schoolauthorization.UserEntity.UserData;
 import com.jwtauth.schoolauthorization.config.UserDataSource.UserDatabaseConnMapper;
 
 @UserDatabaseConnMapper
 public interface UserDataRepository {
+    UserData findUserByUsername(String username);
 }
