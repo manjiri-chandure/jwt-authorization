70bdd4d6241649696a5fb152ae820f99ff4b44ca
manjiri-chandure <chanduremanjiri@gmail.com>
2024-05-31 08:47:21 +0530

    @RetrableTopic annotation removed, exponentialBackOff added in errorHandler and required values to it is initialized in application.properties

--- src/main/java/com/jwtauth/schoolauthorization/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/jwtauth/schoolauthorization/kafka/consumer/KafkaConsumerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.ExponentialBackOff;
 import org.springframework.util.backoff.FixedBackOff;
 import org.springframework.web.client.HttpServerErrorException;
 
@@ -33,29 +34,45 @@ public class KafkaConsumerConfig {
     }
 
 
-//    @Value(value = "${kafka.backoff.interval}")
-//    private Long interval;
-//
-//    @Value(value = "${kafka.backoff.max_failure}")
-//    private Long maxAttempts;
-//
-//
-//    @Bean
-//    public DefaultErrorHandler errorHandler() {
-//        BackOff fixedBackOff = new FixedBackOff(interval, maxAttempts);
-//        DefaultErrorHandler errorHandler;
-//      errorHandler = new DefaultErrorHandler((consumerRecord, exception) -> {
-//          System.out.println("All retry tried");
-//      }, fixedBackOff);
-//      errorHandler.addRetryableExceptions(RuntimeException.class);
-//      return errorHandler;
-//    }
+    @Value(value = "${kafka.backoff.interval}")
+    private Long initialInterval;
+
+    @Value(value = "${kafka.backoff.max_failure}")
+    private Integer attempts;
+
+    @Value(value = "${kafka.backoff.maxElapsedTime}")
+    private Long maxElapsedTime;
+
+    @Value(value = "${kafka.backoff.maxInterval}")
+    private Long maxInterval;
+
+    @Value(value = "${kafka.backoff.multiplier}")
+    private double multiplier;
+
+
+
+
+
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+          ExponentialBackOff expoBackOff = new ExponentialBackOff(initialInterval, multiplier);
+          expoBackOff.setMaxAttempts(attempts);
+          expoBackOff.setMaxElapsedTime(maxElapsedTime);
+          expoBackOff.setMaxInterval(maxInterval);
+
+          DefaultErrorHandler errorHandler;
+          errorHandler = new DefaultErrorHandler((consumerRecord, exception) -> {
+              System.out.println("All retry tried");
+          }, expoBackOff);
+          errorHandler.addRetryableExceptions(RuntimeException.class);
+          return errorHandler;
+    }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, StudentCreationDtoByKafka> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, StudentCreationDtoByKafka> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-//        factory.setCommonErrorHandler(errorHandler());
+        factory.setCommonErrorHandler(errorHandler());
 
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
 
